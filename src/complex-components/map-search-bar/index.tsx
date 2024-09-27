253dd1f7b15fc95983ce4a25e19e2ddb96f45c52
Marty2390 <129055675+Marty2390@users.noreply.github.com>
2024-09-27 13:08:49 +0100

    add search results

--- src/complex-components/map-search-bar/index.tsx
+++ src/complex-components/map-search-bar/index.tsx
@@ -1,39 +1,87 @@
-import { Button, TextInput } from '@src/components';
-import React, { useCallback, useState } from 'react';
+// React
+import React, { useCallback, useEffect, useState } from 'react';
+// Native
 import { View } from 'react-native';
+// Components
+import { Button, Text, TextInput } from '@src/components';
+// Store
 import * as Store from '@store/index';
+// Hooks
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useStyles } from './styles';
+// Animations
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 
 export const MapSearchBar = (): React.ReactElement => {
   const dispatch = Store.useDispatch();
   const [query, setQuery] = useState('');
 
+  const insets = useSafeAreaInsets();
+  const styles = useStyles(insets);
+
+  const autoCompleteResults = Store.useSelector(
+    (store) => store.markers.autoCompleteResults,
+  );
+
   const handleSearch = useCallback((): void => {
     if (!query) return;
 
-    dispatch(Store.Markers.handleSearch({ query })).then(() => {
-      setQuery('');
-    });
+    dispatch(Store.Markers.handleSearch({ query })).then(() => setQuery(''));
+  }, [dispatch, query]);
+
+  const handleTextChange = (e: string): void => {
+    if (!e) {
+      dispatch(Store.Markers.clearAutocomplete());
+    }
+
+    setQuery(e);
+  };
+
+  // Debounce to wait for user input to end for optimisation
+  useEffect(() => {
+    if (!query) return;
+
+    const delayDebounceFn = setTimeout(() => {
+      dispatch(Store.Markers.getAutoCompleteResults({ query }));
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
   }, [dispatch, query]);
 
   return (
-    <View
-      style={{
-        paddingHorizontal: 20,
-        position: 'absolute',
-        zIndex: 999,
-        top: 50,
-        flexDirection: 'row',
-        gap: 12,
-        width: '100%',
-      }}>
-      <TextInput
-        placeholder="Search"
-        value={query}
-        onChangeText={(e) => setQuery(e)}
-        rootStyle={{ flex: 1 }}
-        multiline={false}
-      />
-      <Button text="search" onPress={handleSearch} />
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Search"
+          variant="filled"
+          size="small"
+          value={query}
+          onChangeText={(e) => handleTextChange(e)}
+          rootStyle={styles.input}
+          multiline={false}
+        />
+        <Button text="Search" size="small" onPress={handleSearch} />
+      </View>
+      {autoCompleteResults && (
+        <Animated.View
+          entering={FadeIn}
+          exiting={FadeOut}
+          style={styles.autoComplete}>
+          {autoCompleteResults?.map((item, i) => (
+            <View
+              key={item.place_id}
+              style={{
+                ...styles.autoCompleteItem,
+                paddingBottom:
+                  i === autoCompleteResults.length - 1
+                    ? 0
+                    : styles.autoCompleteItem.paddingBottom,
+              }}>
+              <Text variant="body-small">{item.description}</Text>
+            </View>
+          ))}
+        </Animated.View>
+      )}
     </View>
   );
 };
---
+++ src/complex-components/map-search-bar/styles.ts
@@ -0,0 +1,32 @@
+import { baseTheme } from '@src/theme';
+import { getSpacing } from '@src/theme/utils/spacing';
+import { StyleSheet } from 'react-native';
+import { EdgeInsets } from 'react-native-safe-area-context';
+
+export const useStyles = (insets: EdgeInsets) =>
+  StyleSheet.create({
+    root: {
+      paddingHorizontal: getSpacing(5, 'width'),
+      position: 'absolute',
+      zIndex: 999,
+      top: insets.top,
+      width: '100%',
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      gap: getSpacing(3, 'width'),
+    },
+    input: {
+      flex: 1,
+    },
+    autoComplete: {
+      marginTop: getSpacing(3, 'height'),
+      paddingHorizontal: getSpacing(3, 'width'),
+      paddingVertical: getSpacing(4, 'height'),
+      backgroundColor: baseTheme.colors.white,
+      borderRadius: getSpacing(4, 'width'),
+    },
+    autoCompleteItem: {
+      paddingBottom: getSpacing(3, 'height'),
+    },
+  });
